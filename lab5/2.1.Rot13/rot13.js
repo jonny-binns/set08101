5eade41321fb3ee9ea67eb5c38a57e30529fd66c
Jonny <jonny.binns1@gmail.com>
2019-02-05 16:42:10 +0000

    added lab 5

---
+++ lab5/2.1.Rot13/rot13.js
@@ -0,0 +1,8 @@
+function encode(){
+    var str = document.getElementById("message").value;
+    var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    var index     = x => input.indexOf(x);
+    var translate = x => index(x) > -1 ? output[index(x)] : x;
+    document.getElementById("output").innerHTML = str.split('').map(translate).join('');
+}
